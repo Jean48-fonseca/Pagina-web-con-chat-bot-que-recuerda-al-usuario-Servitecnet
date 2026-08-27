b8331890059d18cd2d6d0d41d9867b9f5acb9f2b
Jean Fonseca <cayth78@gmail.com>
2026-08-27 15:18:21 -0500

    correccion de tipeo en suppor faltaba una p

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Suport\Facades\URL;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
