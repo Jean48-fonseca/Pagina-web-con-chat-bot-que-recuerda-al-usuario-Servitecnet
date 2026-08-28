a0cc3c4c0b1498dd671b479c695f5babd0299c13
Jean Fonseca <cayth78@gmail.com>
2026-08-27 23:07:16 -0500

    falto eliminar el env

--- config/services.php
+++ config/services.php
@@ -37,6 +37,6 @@ return [
     'google' =>[
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('https://servitecnet.onrender.com/auth/google/callback'),
+        'redirect' => 'https://servitecnet.onrender.com/auth/google/callback',
     ]
 ];
