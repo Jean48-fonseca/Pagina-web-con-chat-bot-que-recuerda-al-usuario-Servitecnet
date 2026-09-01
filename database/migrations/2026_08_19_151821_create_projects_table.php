c360ccc11553a6dee5c7f22cefb4704dbe56692a
Jean Fonseca <cayth78@gmail.com>
2026-09-01 14:29:23 -0500

    fix: inyectar columnas faltantes

--- database/migrations/2026_08_19_151821_create_projects_table.php
+++ database/migrations/2026_08_19_151821_create_projects_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('title'); // Título del proyecto
             $table->text('description'); // Descripción del proyecto
             $table->string('image_path')->nullable(); // Ruta de la foto
+            $table->string('encargado'); // Nombre del encargado
+            $table->string('modelo_3d_ruta')->nullable(); // Ruta del modelo 3D
+            $table->boolean('es_destacado')->default(0)->after('modelo_3d_ruta'); // Indica si el proyecto es destacado (1) o no (0)
             $table->timestamps(); // Esto crea las columnas created_at y updated_at
         });
     }
@@ -25,6 +28,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn(['encargado', 'modelo_3d_ruta', 'es_destacado']);
+        });
     }
 };
